c50da0496cc62ff83760ffcb67069bdfe406779b
ER-RADA Oumaima <erradaoumaima@gmail.com>
2023-12-24 16:28:44 +0100

    update

--- app/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'config/config.php';
-require_once 'helpers/url_helper.php';
-
-spl_autoload_register(function ($className) {
-    require 'libraries/' . $className . '.php';
-});
-
